54162b23d2db58a6807c949d40d9adc6f65241e6
Irene Elizabeth Sabu <56963461+ireneelizabethsabu@users.noreply.github.com>
2021-05-26 01:45:37 +0530

    express-session added, fixed scheduler

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ const PORT = process.env.PORT || 3001;
 const moment = require("moment");
 var  botFn  = require('./src/botHelpers');
 var  db  = require('./src/database');
+var session = require('express-session');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(session({
+  secret: process.env.SESSION,
+  resave: true,
+  saveUninitialized: true
+}));
+
+function checkSignIn(req, res){
+   if(req.session.user){
+      next();     //If session exists, proceed to page
+   } else {
+      //var err = new Error("Not logged in!");
+      console.log('please sign to go to dashboard');
+      //next(err);  //Error, trying to access unauthorized page!
+   }
+}
 
 const server = app.listen(PORT, () => {
   console.log("App listening on port " + PORT);
@@ -16,7 +35,6 @@ const server = app.listen(PORT, () => {
 
 const io = require('socket.io')(server);
 app.set('socketio', io);
-
 
 //app.use
 app.use(express.static(__dirname + "/front-end/"));
@@ -34,7 +52,7 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/front-end/index.html");
 });
 
-app.get("/dashboard", (req, res) => {
+app.get("/dashboard",checkSignIn, (req, res) => {
   res.sendFile(__dirname + "/front-end/dashboard.html");
 });
 
@@ -59,6 +77,7 @@ app.get("/oauth", function (req, res) {
           res.redirect("/");
         } else {
           body = JSON.parse(body);
+          req.session.user = body.authed_user.id;
           db.addUserToDatabase(body,res);
         }
       } 
@@ -68,19 +87,21 @@ app.get("/oauth", function (req, res) {
 
 //app.post()
 app.post("/setreminder", urlencodedParser, (req, res) => {
-  db.reminderData(req.body);
+  db.reminderData(req.body,req.session.user);
   var s=req.body.startime+":00";
   var l=req.body.lastime+":00";
   var start=moment(s,"HH:mm");
   var end = moment(l,"HH:mm");
   var duration = moment.duration(end.diff(start));
-
-  db.findInUser().then((docs) => {
+  db.findInUser(req.session.user).then((docs) => {
     var interval = parseInt(JSON.stringify(req.body).split(" ")[1]);
-    interval = (interval === 30) ? interval*60000 : interval* 3600000;
-    function timingFn () {  
-      if (moment().format('h:mm').isBetween(req.body.startime,req.body.lastime)){
+    interval = (interval === 30 ) ? interval*60000 : interval* 3600000;
+    function timingFn () {
+      var t = moment().utcOffset("+05:30").format("HH:mm");
+      var time=moment(t,"HH:mm");
+      if (time.isBetween(start,end)){
         botFn.publishMessage(docs.channelId,docs.userId);
+        console.log('msg incoming');
         setTimeout(timingFn,interval);
       }else{
         console.log('its sleep time');
@@ -120,9 +141,7 @@ app.post("/interactive", urlencodedParser, (req, res) => {
           { text: "Your feedback is saved! :+1:" },
           { fallback: "buttons aren't supported in this land " },
         ]
-      });  
-         
+      });   
       res.status(200).end();
   }
 });
-
